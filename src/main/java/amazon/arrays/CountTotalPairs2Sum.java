7f366b362a10c5528d5d3abcf1955964fa0d4529
tmdtest1234 <tmdtest1234@gmail.com>
2021-05-07 08:06:03 -0700

    CountTotalPairs2Sum
    Signed-off-by: tmdtest1234 <tmdtest1234@gmail.com>

---
+++ src/main/java/amazon/arrays/CountTotalPairs2Sum.java
@@ -0,0 +1,34 @@
+package amazon.arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * Created by Ashok Kolli on 5/6/2021
+ */
+
+public class CountTotalPairs2Sum {
+    public static void main(String[] args) {
+        int[] arr = {0, -1, 2, -3, 1};
+        int sum = -2;
+        countTotalPair2Sum(arr, sum);
+    }
+
+    public static void countTotalPair2Sum(int[] arr, int sum) {
+        Map<Integer, HashSet<Integer>> map;
+        Arrays.sort(arr);
+        int l = 0;
+        int r = arr.length - 1;
+        while (l < r) {
+            if (arr[l] + arr[r] > sum)
+                r--;
+            else if (arr[l] + arr[r] < sum)
+                l++;
+            else {
+                System.out.println(arr[l] + " " + arr[r]);
+                l++;
+            }
+        }
+    }
+}
