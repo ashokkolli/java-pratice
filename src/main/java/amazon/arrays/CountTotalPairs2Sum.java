671c4037b76726bfe705ede576c51bf276112f14
tmdtest1234 <tmdtest1234@gmail.com>
2021-06-11 08:34:31 -0700

    Strings programs practice
    Signed-off-by: tmdtest1234 <tmdtest1234@gmail.com>

--- src/main/java/amazon/arrays/CountTotalPairs2Sum.java
+++ src/main/java/amazon/arrays/CountTotalPairs2Sum.java
@@ -1,8 +1,6 @@
 package amazon.arrays;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by Ashok Kolli on 5/6/2021
@@ -16,8 +14,7 @@ public class CountTotalPairs2Sum {
     }
 
     public static void countTotalPair2Sum(int[] arr, int sum) {
-        Set<HashSet<Integer>> setCollection = new HashSet<>();
-        Set<Integer> sets = new HashSet<>();
+
         Arrays.sort(arr);
         int l = 0;
         int r = arr.length - 1;
