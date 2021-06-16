1789e633edcc9066ac32b11327056195a3b0af03
AK110849 <ashokkumar.kolli@gmail.com>
2021-06-15 18:29:25 -0700

    TreeMap and MinimumWindow

---
+++ src/main/java/amazon/collections/TreeMapCollection.java
@@ -0,0 +1,20 @@
+package amazon.collections;
+import java.util.TreeMap;
+import java.util.Map;
+
+public class TreeMapCollection {
+
+    public static void main(String[] args) {
+        TreeMap<Integer, String> tree_map = new TreeMap<>();
+        tree_map.put(25, "Welcomes");
+        tree_map.put(30, "You");
+        tree_map.put(10, "Geeks");
+        tree_map.put(15, "4");
+        tree_map.put(20, "Geeks");
+
+        //tree_map.
+        for(Map.Entry<Integer, String> entry : tree_map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
