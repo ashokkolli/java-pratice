556fcb90e686db7c55576512322ca8c4662aafe0
Ashok <ashokkumar.kolli@gmail.com>
2021-07-14 08:56:51 -0700

    AllAnagramsInString

---
+++ src/main/java/apple/strings/AllAnagramsInString.java
@@ -0,0 +1,62 @@
+package apple.strings;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AllAnagramsInString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        int ns = s.length(), np = p.length();
+        if (ns < np)
+            return new ArrayList();
+
+        Map<Character, Integer> pCount = new HashMap<>();
+        Map<Character, Integer> sCount = new HashMap<>();
+
+        //build reference hashmap using String p
+        for (char ch : p.toCharArray()) {
+            if (pCount.containsKey(ch)) {
+                pCount.put(ch, pCount.get(ch) + 1);
+            } else {
+                pCount.put(ch, 1);
+            }
+        }
+
+        List<Integer> output = new ArrayList<>();
+        //Sliding window on the String S
+
+        for (int i = 0; i < ns; i++) {
+            //Add one more letter on the right side of the windows
+            char ch = s.charAt(i);
+            if (sCount.containsKey(ch)) {
+                sCount.put(ch, sCount.get(ch) + 1);
+            } else {
+                sCount.put(ch, 1);
+            }
+
+            //remove one letter from hte left side of the window
+            if (i >= np) {
+                ch = s.charAt(i - np);
+                if (sCount.get(ch) == 1) {
+                    sCount.remove(ch);
+                } else {
+                    sCount.put(ch, sCount.get(ch) - 1);
+                }
+            }
+
+
+            if (pCount.equals(sCount)) {
+                output.add(i - np + 1);
+            }
+
+        }
+        return output;
+
+    }
+}
