d3126d991c7141cb56ab0f83e82f91a08f587fda
Ashok <ashokkumar.kolli@gmail.com>
2021-07-16 13:40:09 -0700

    AllAnagramsInString

--- src/main/java/apple/strings/slidingwindow/AllAnagramsInString.java
+++ src/main/java/apple/strings/slidingwindow/AllAnagramsInString.java
@@ -8,14 +8,16 @@ import java.util.Map;
 public class AllAnagramsInString {
 
     public static void main(String[] args) {
+        String s = "abab", p = "ab";
+        System.out.println(findAnagrams(s, p));
 
     }
 
     public static List<Integer> findAnagrams(String s, String p) {
         int ns = s.length(), np = p.length();
         if (ns < np)
-            return new ArrayList<Integer>();
-
+            return new ArrayList<>();
+        int i = 0;
         Map<Character, Integer> pCount = new HashMap<>();
         Map<Character, Integer> sCount = new HashMap<>();
 
@@ -31,28 +33,27 @@ public class AllAnagramsInString {
         List<Integer> output = new ArrayList<>();
         //Sliding window on the String S
 
-        for (int i = 0; i < ns; i++) {
+        for (int j = 0; j < ns; j++) {
             //Add one more letter on the right side of the windows
-            char ch = s.charAt(i);
+            char ch = s.charAt(j);
             if (sCount.containsKey(ch)) {
                 sCount.put(ch, sCount.get(ch) + 1);
             } else {
                 sCount.put(ch, 1);
             }
 
-            //remove one letter from hte left side of the window
-            if (i >= np) {
-                ch = s.charAt(i - np);
+            //remove one letter from the left side of the window
+            if (j >= np) {
+                ch = s.charAt(i);
                 if (sCount.get(ch) == 1) {
                     sCount.remove(ch);
                 } else {
                     sCount.put(ch, sCount.get(ch) - 1);
                 }
+                i++;
             }
-
-
             if (pCount.equals(sCount)) {
-                output.add(i - np + 1);
+                output.add(i);
             }
 
         }
