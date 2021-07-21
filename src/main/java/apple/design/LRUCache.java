e44f36bfdbed5569ca6e91a88635d29e5bf2963a
Ashok <ashokkumar.kolli@gmail.com>
2021-07-20 22:18:03 -0700

    MyPower

--- src/main/java/apple/design/LRUCache.java
+++ src/main/java/apple/design/LRUCache.java
@@ -21,15 +21,29 @@ public class LRUCache {
         }
 
     public static void main(String[] args) {
-        LRUCache lruCache = new LRUCache(2);
 
-        System.out.println(lruCache.get(2));
-        lruCache.put(2, 1);
-        lruCache.put(3, 2);
-        lruCache.put(4, 3);
-        System.out.println(lruCache.get(2));
-        System.out.println(lruCache.get(3));
-        lruCache.put(5, 4);
+
+        LRUCache lruCache = new LRUCache(2);
+        System.out.println(lruCache.get(2)); // -1
+        lruCache.put(2, 1); // 2, 1 -
+        lruCache.put(3, 2); // (2, 1), (3, 2)
+        lruCache.put(4, 3); // (2, 1), (3, 2), (4, 3) --eldestRemove--> (3, 2), (4, 3)
+        System.out.println(lruCache.get(2)); // -1
+        for(Map.Entry<Integer, Integer> entrySet : lruCache.map.entrySet())
+            System.out.println("key: "+entrySet.getKey());
+
+        System.out.println(lruCache.get(3)); // 2 //  reorder cache-->  (4, 3), (3, 2)
+
+        /**
+         * ************ LinkedHashMap reorders itself based on last visited/get key
+         **/
+
+
+        for(Map.Entry<Integer, Integer> entrySet : lruCache.map.entrySet())
+            System.out.println("key: "+entrySet.getKey());
+
+
+        lruCache.put(5, 4); //
         System.out.println(lruCache.map.size());
         System.out.println(lruCache.get(4));
 
---
+++ src/main/java/apple/generic/MyPower.java
@@ -0,0 +1,20 @@
+package apple.generic;
+
+public class MyPower {
+
+    public static void main(String[] args){
+        System.out.println(myPow(9,3));
+    }
+
+    public static long  myPow(int x, int n){
+        if(n == 0)
+            return  1;
+        else if( n%2 == 0){
+            long y = myPow(x, n/2);
+            return y*y;
+        }else{
+            return x*myPow(x, n-1);
+        }
+  }
+
+}
