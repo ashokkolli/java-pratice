9188db59d5f17d4047ab30286558b673ba12d43e
Ashok <ashokkumar.kolli@gmail.com>
2021-10-03 08:15:22 -0700

    NumberToArray

--- src/main/java/core/apple/arrays/leetcode/easy/NumberToArray.java
+++ src/main/java/core/apple/arrays/leetcode/easy/NumberToArray.java
@@ -2,18 +2,27 @@ package core.apple.arrays.leetcode.easy;
 
 public class NumberToArray {
     public static void main(String[] args) {
-        lenOfNumber(1234);
+        numberToArray(1234567890);
     }
-  /*  public int[] numberToArray(Integer num){
+  public static int[] numberToArray(Integer num){
+      int len = lenOfNumber(num);
+      int[] arr = new int[len];
+      while (num!=0){
+          arr[--len] = num%10;
+          num = num/10;
+      }
+      for (int i = 0; i < arr.length; i++) {
+          System.out.println(arr[i]);
+      }
+      return arr;
 
-    }*/
+    }
     public static int lenOfNumber(Integer  num){
         Integer count =0;
         while(num!=0){
             num = num/10;
             count++;
         }
-        System.out.println(count);
         return count;
     }
 
