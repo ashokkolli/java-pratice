8ee8235aea49672bda6519795ffc4c6c5d1979b1
Ashok Kolli <tmdtest1234@gmail.com>
2021-11-15 22:23:29 -0500

    Single Linked List

---
+++ src/main/java/core/imp/linkedlist/DetectAndRemoveLoop.java
@@ -0,0 +1,54 @@
+package core.imp.linkedlist;
+
+public class DetectAndRemoveLoop {
+    static Node head;
+
+    public static void main(String[] args) {
+        DetectAndRemoveLoop drl = new DetectAndRemoveLoop();
+        //Creating Nodes;
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        //Creating a loop
+        head.next.next.next.next.next.next = head.next.next;
+    }
+
+    public static void detectAndRemoveLoop(Node node) {
+        Node slow = node, fast = node;
+        Node start;
+        //If fast.next is null, then there is no loop
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                //removeLoop(slow, node);
+                break;
+            }
+
+        }
+        fast = head;
+        while (slow != head) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        start = slow;
+    }
+
+    public static void removeLoop(Node slow, Node curr) {
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+}
+
---
+++ src/main/java/core/imp/linkedlist/Testing.java
@@ -0,0 +1,61 @@
+package core.imp.linkedlist;
+
+public class Testing {
+// Java program for reversing the linked list
+
+
+    static Node head;
+
+    public static void main(String[] args) {
+        Testing list = new Testing();
+        head = new Node(85);
+        head.next = new Node(15);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(20);
+
+        System.out.println("Given Linked list");
+        list.printList(head);
+        head = list.reverse(head);
+        System.out.println();
+        System.out.println("Reversed linked list ");
+        list.printList(head);
+    }
+
+    /* Function to reverse the linked list */
+    Node reverse(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+
+    // prints content of double linked list
+    void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+}
+
+
+// This code has been contributed by Mayank Jaiswal
+
