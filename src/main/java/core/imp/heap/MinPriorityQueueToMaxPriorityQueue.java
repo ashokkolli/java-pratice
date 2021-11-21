9f69faa6451e3ed78092ab99686ccbfdb43afddd
Ashok Kolli <tmdtest1234@gmail.com>
2021-11-20 20:22:59 -0800

    Heaps

---
+++ src/main/java/core/imp/heap/MinPriorityQueueToMaxPriorityQueue.java
@@ -0,0 +1,19 @@
+package core.imp.heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MinPriorityQueueToMaxPriorityQueue {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 0};
+        int k = 2;
+        //Comparator.reverseOrder()
+        PriorityQueue<Integer> pr = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        for (int ele : arr) {
+            pr.add(ele);
+            System.out.println(pr);
+        }
+    }
+
+}
