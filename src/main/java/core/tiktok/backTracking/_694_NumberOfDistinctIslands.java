f8972e2c8bb5c6fc9655701476ba4f81c440a9dd
Ashok Kolli <tmdtest1234@gmail.com>
2021-12-27 22:16:52 -0800

    NumberOfDistinctIslands

--- src/main/java/core/tiktok/backTracking/_694_NumberOfDistinctIslands.java
+++ src/main/java/core/tiktok/backTracking/_694_NumberOfDistinctIslands.java
@@ -4,6 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _694_NumberOfDistinctIslands {
+    //Complexity Analysis
+
+//Let M be the number of rows, and N be the number of columns.
+
+//Time Complexity : O((M⋅N).
+
+//Space complexity : O((M⋅N)
+
 
     // S - Starting Point
     // O - Out of bounds
@@ -35,16 +43,15 @@ public class _694_NumberOfDistinctIslands {
 
         int nr = grid.length;
         int nc = grid[0].length;
-        if(r < 0 || r >= nr || c < 0 || c >= nc || grid[r][c] != 1)
+        if(r < 0 || r == nr || c < 0 || c == nc || grid[r][c] != 1)
             return "O";
         grid[r][c] = 2;
-        String left = computePath(grid, r, c, "L");
-        String right = computePath(grid, r, c, "R");
-        String down = computePath(grid, r, c, "D");
-        String up = computePath(grid, r, c, "U");
+        String left = computePath(grid, r , c - 1, "L");
+        String right = computePath(grid, r, c +1, "R");
+        String down = computePath(grid, r + 1, c, "D");
+        String up = computePath(grid, r - 1, c, "U");
         return direction + left + right + down + up;
 
     }
-
 
 }
